8ebecfc8972f42b695609b9711d33735f9d0b3d9
Robin Lee Powell <rlpowell@digitalkingdom.org>
2022-07-30 16:11:56 -0700

    More stuff, clearer order
    An attempt to download as much of planecrash as I can find, with some
    attempt at clarity as to which order they should be read in.

--- examples/planecrash.rs
+++ examples/planecrash.rs
@@ -1,30 +1,38 @@
 use clap::Parser;
 use std::{collections::BTreeSet, path::PathBuf};
+use slug::slugify;
 
 use glowfic_to_epub::Thread;
 
-pub const IDS: [&[u64]; 2] = [&MAIN, &SANDBOXES];
-
-/// Main planescrash section
-pub const MAIN: [u64; 10] = [
-    4582, // mad investor chaos and the woman of asmodeus
-    5504, // some human relationships are less universal than others
-    5506, // take this report back and bring her a better report
-    5508, // project lawful and their oblivious boyfriend
-    5694, // my fun research project has more existential risk than I anticipated
-    5930, // what the truth can destroy
-    5977, // crisis of faith
-    6075, // the woman of irori
-    6131, // flashback: this is not a threat
-    6132, // null action
-];
-
-/// planecrash sandboxes
-pub const SANDBOXES: [u64; 4] = [
-    5775, // totally not evil
-    5778, // welcome to project lawful
-    5880, // I reject your alternate reality and substitute my own
-    6124, // dear abrogail
+// This is my (Robin Lee Powell)'s best guess as to the correct reading order, given that I haven't
+// actually read most of it yet, and given that in several places the text itself tells you to jump
+// out to a subthread and come back, which makes "reading order" a bit of a miss anyway.
+pub const ITEMS: [(&str, u64); 25] = [
+    ("01", 4582), // MAIN: mad investor chaos and the woman of asmodeus
+    ("01-subthread-01", 5310), // SUBTHREAD: kissing is not a human universal
+    ("01-subthread-01-sfw-tldr", 5403), // ALTERNATE SUBTHREAD: sfw tldr kissing is not a human universal
+    ("02-and-03-alternate", 5521), // ALTERNATE, less disturbing, for "some human" and "take this report"
+    ("02", 5504), // MAIN: some human relationships are less universal than others
+    ("03", 5506), // MAIN: take this report back and bring her a better report
+    ("04", 5508), // MAIN: project lawful and their oblivious boyfriend
+    ("04-subthread-01", 5610), // SUBTHREAD: cheating is cuddleroom technique
+    ("04-subthread-01-sfw-tldr", 5618), // ALTERNATE SUBTHREAD: sfw tldr cheating is cuddleroom technique
+    ("04-subthread-02", 5638), // SUBTHREAD: in another world we could have been trade partners
+    ("04-subthread-02-sfw-tldr", 5671), // ALTERNATE SUBTHREAD: sfw tldr we could have been trade partners
+    ("04-sandbox-01", 5775), // SANDBOX: totally not evil
+    ("04-sandbox-02", 5778), // SANDBOX: welcome to project lawful
+    ("05", 5694), // MAIN: my fun research project has more existential risk than I anticipated
+    ("05-subthread-01", 5785), // SUBTHREAD / LECTURE: to hell with science
+    ("05-subthread-02", 5826), // SUBTHREAD SORT OF?: to earth with science
+    ("05-subthread-03", 5864), // SUBTHREAD / LECTURE: the alien maths of dath ilan
+    ("05-sandbox-01", 5880), // SANDBOX: I reject your alternate reality and substitute my own
+    ("06", 5930), // MAIN: what the truth can destroy
+    ("06-sandbox-01", 6029), // SANDBOX: it is a beautiful day in Cheliax and you are a horrible medianworld romance novel
+    ("07", 5977), // MAIN: crisis of faith
+    ("08", 6075), // MAIN: the woman of irori
+    ("08-sandbox-01", 6124), // SANDBOX: dear abrogail
+    ("09", 6131), // MAIN: flashback: this is not a threat
+    ("10", 6132), // MAIN: null action
 ];
 
 /// Download and process a all glowfic posts in the planecrash series.
@@ -45,19 +53,19 @@ async fn main() {
 
     let mut threads = vec![];
 
-    for id in IDS.into_iter().flatten().copied() {
+    for (preamble, id) in ITEMS {
         println!("Downloading post {id}");
 
         let thread = Thread::get_cached(id, !use_cache).await.unwrap().unwrap();
 
         println!("Downloaded post {id} - {}", &thread.post.subject);
 
-        threads.push(thread);
+        threads.push((preamble, thread));
     }
 
     let icons: BTreeSet<_> = threads
         .iter()
-        .flat_map(|thread| thread.icons())
+        .flat_map(|(_, thread)| thread.icons())
         .cloned()
         .collect();
 
@@ -67,21 +75,22 @@ async fn main() {
         }
     }
 
-    for thread in threads {
+    for (preamble, thread) in threads {
         let post_id = thread.post.id;
+        let title_slug = slugify(&thread.post.subject);
 
         {
-            println!("Generating html document {post_id}...");
+            println!("Generating html document {preamble}_{post_id}_{title_slug}...");
 
-            let path = PathBuf::from(format!("./books/html/{post_id}.html"));
+            let path = PathBuf::from(format!("./books/html/{preamble}_{post_id}_{title_slug}.html"));
             std::fs::create_dir_all(path.parent().unwrap()).unwrap();
             std::fs::write(path, thread.to_single_html_page(for_tts)).unwrap();
         }
 
         {
-            println!("Generating epub document {post_id}...");
+            println!("Generating epub document {preamble}_{post_id}_{title_slug}...");
 
-            let path = PathBuf::from(format!("./books/epub/{post_id}.epub"));
+            let path = PathBuf::from(format!("./books/epub/{preamble}_{post_id}_{title_slug}.epub"));
             std::fs::create_dir_all(path.parent().unwrap()).unwrap();
             std::fs::write(path, &thread.to_epub(for_tts).await.unwrap()).unwrap();
         }
